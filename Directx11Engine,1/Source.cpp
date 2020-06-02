6bfdc360761ef662f63922a523feb6a95c53b3d6
burnboy <uty1993@naver.com>
2020-06-02 19:09:08 +0900

    다렉 윈도우 콘솔창 띄우기
    윈도우 콘솔창 띄우기

---
+++ Directx11Engine,1/Source.cpp
@@ -0,0 +1,31 @@
+#include"RenderWindow.h"
+#pragma comment(lib,"d3d11.lib")
+#pragma comment(lib,"DirectXTK.lib")
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int	nCmdShow)
+{
+	HRESULT hr = S_OK;
+	if (SUCCEEDED(hr))
+	{
+		MessageBoxA(NULL, "SUCCESS", "SUCCESS", NULL);
+	
+	}
+	//ShowWindow(SW_SHOW)
+	/*if (FAILED(hr))
+	{
+		ErrorLogger::Log(hr, "FAILURE");
+
+	}*/
+	//ErrorLogger::Log(E_INVALIDARG, "Test Message");
+
+	RenderWindow rw;
+	rw.Initialize(hInstance, "Title", "MyWindowClass", 800, 600);
+	while (rw.ProcessMessages()==true)
+	{
+		//Sleep(50);
+	}
+	return 0;
+}
