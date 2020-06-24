d9ce9376265aa92072c76714a7a9ca8ca763e8cd
burnboy <uty1993@naver.com>
2020-06-25 02:05:10 +0900

    마우스 입력-2

--- Directx11Engine,1/Mouse/MouseEvent.h
+++ Directx11Engine,1/Mouse/MouseEvent.h
@@ -29,7 +29,14 @@ private:
 	int x;
 	int y;
 
+public:
 	MouseEvent();
-	~MouseEvent();
+	MouseEvent(const EventType type, const int x, const int y);
+	bool IsValid() const;
+	EventType GetType() const;
+	MousePoint GetPos() const;
+	int GetPosX() const;
+	int GetPosY() const;
+
 };
 
